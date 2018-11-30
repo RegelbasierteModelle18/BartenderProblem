8d034c8333d81e1e9548bf3ebcbb3ad48e519c65
Malte kl. Piening <mkl.piening@gmail.com>
2018-11-30 11:11:50 +0100

    created main structure

---
+++ src/bartenderProblem/BartenderBuilder.java
@@ -0,0 +1,32 @@
+package bartenderProblem;
+
+import repast.simphony.context.Context;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.grid.GridFactory;
+import repast.simphony.context.space.grid.GridFactoryFinder;
+import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.RandomCartesianAdder;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridBuilderParameters;
+import repast.simphony.space.grid.RandomGridAdder;
+
+public class BartenderBuilder implements ContextBuilder<Object> {
+	public Context<Object> build(Context<Object> context) {
+		int xdim = 100, ydim = 100;
+		
+		// grid for environment
+		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
+		Grid<Object> grid = gridFactory.createGrid("Simple Grid", context,
+				new GridBuilderParameters<Object>(new repast.simphony.space.grid.BouncyBorders(),
+						new RandomGridAdder<Object>(), true, xdim, ydim));
+		
+		// continuous space for agents walking in the environment
+		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
+		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace("Continuous Space", context, new RandomCartesianAdder<Object>(),
+				new repast.simphony.space.continuous.BouncyBorders(), xdim, ydim, 1);
+		
+		return context;
+	}
+}
