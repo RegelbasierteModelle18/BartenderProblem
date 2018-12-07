1934599fd840aa28d973489231885018437c4edb
Malte kl. Piening <mkl.piening@gmail.com>
2018-12-07 11:32:29 +0100

    added method for bartender interface

--- src/bartenderProblem/actors/bartender/Bartender.java
+++ src/bartenderProblem/actors/bartender/Bartender.java
@@ -1,5 +1,6 @@
 package bartenderProblem.actors.bartender;
 
+import bartenderProblem.actors.Guest;
 import repast.simphony.context.Context;
 import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.space.continuous.ContinuousSpace;
@@ -14,10 +15,14 @@ public abstract class Bartender {
 		Grid<Object> grid = (Grid<Object>) context.getProjection("Simple Grid");
 		ContinuousSpace<Object> space = (ContinuousSpace<Object>) context.getProjection("Continuous Space");
 				
+		// TODO: check for nearby guests
+		
 		space.moveByVector(this, 1, calculateHeading(),0,0);
 		NdPoint point = space.getLocation(this);
 		grid.moveTo(this, (int) point.getX(), (int) point.getY());
 	}
 	
 	protected abstract double calculateHeading();
+
+	protected abstract void handlePersonCommunication(Guest guest);
 }
--- src/bartenderProblem/actors/bartender/StupidBartender.java
+++ src/bartenderProblem/actors/bartender/StupidBartender.java
@@ -1,7 +1,16 @@
 package bartenderProblem.actors.bartender;
 
+import bartenderProblem.actors.Guest;
+import repast.simphony.context.Context;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
+
 public class StupidBartender extends Bartender {
 	protected double calculateHeading() {
 		return 0;
+	}
+	
+	protected void handlePersonCommunication(Guest guest) {
+		// take order or hand over drink
 	}
 }
